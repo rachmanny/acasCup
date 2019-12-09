caa6bfde2751c71418dec316592c83146ce36f67
David Vetan <david.vetan@bgasoft.com>
2019-12-09 15:29:00 -0500

    update front end - something off with the placement of the data inside the columns

--- index.php
+++ index.php
@@ -5,25 +5,36 @@
   <h1 align=center>ACAS Cup 2020</h1>
   <h2 align=center>Goal: Raise funds to cover the cost of the building.</h2>
 
-  <?php
-    echo "bro this works?";
-  ?>
-
   <table style=width:100%>
     <tr>
-      <th>Groups</th>
+      <th>Group Draw</th>
       <th>Schedule and Scores</th>
       <th>Top Scorers</th>
     <tr>
     <tr align=center>
       <td>
-        <table id=groupA>
+        <table id=groups>
           <tr>
             <th>GROUP A</th>
+            <th>GROUP B</th>
           </tr>
           <tr>
-            <td>Harul FC</td>
+            <td>1. Harul FC</td>
+            <td>1. FC Romania</td>
           </tr>
+          <tr>
+            <td>2. Red Bulls FC</td>
+            <td>2. Copacei FC</td>
+          </tr> 
+          <tr>
+            <td>3. Colombia</td>
+            <td>3. Poli</td>
+          </tr>
+          <tr>
+            <td>4. Hillcrest FC</td>
+            <td>4. Hollywood FC</td>
+          </tr>
+
         </table>
       </td>
 
@@ -33,8 +44,27 @@
         <table>
           <tr><td>8:45AM - Registration</td></tr>
           <tr><td>9:15AM - Rules</td></tr>
-          <tr><td>9:30AM - First Match Kick Off</td></tr>
+          <tr><td>9:30AM - First Match Kick Off : 1 vs 2</td></tr>
           <tr><td>9:55AM - GROUP A Match : 3 vs 4</td></tr>
+          <tr><td>10:20AM - GROUP B Match : 1 vs 2</td></tr>
+          <tr><td>10:45AM - GROUP B Match : 3 vs 4</td></tr>
+          <tr><td>11:10AM - GROUP A Match : 1 vs 3</td></tr>
+          <tr><td>11:35AM - GROUP A Match : 2 vs 4</td></tr>
+          <tr><td>12:00PM - GROUP B Match : 1 vs 3</td></tr>
+          <tr><td>12:25PM - GROUP B Match : 2 vs 4</td></tr>
+          <tr><td>12:50PM - GROUP A Match : 1 vs 4</td></tr>
+          <tr><td>1:15PM - GROUP A Match : 2 vs 3</td></tr>
+          <tr><td>1:40PM - GROUP B Match : 1 vs 4</td></tr>
+          <tr><td>2:05PM - GROUP B Match : 2 vs 3</td></tr>
+
+          <tr><td>11:35AM - GROUP A Match : 2 vs 4</td></tr>
+          <tr><td>12:00PM - GROUP B Match : 1 vs 3</td></tr>
+          <tr><td>12:25PM - GROUP B Match : 2 vs 4</td></tr>
+          <tr><td>12:50PM - GROUP A Match : 1 vs 4</td></tr>
+          <tr><td>1:15PM - GROUP A Match : 2 vs 3</td></tr>
+          <tr><td>1:40PM - GROUP B Match : 1 vs 4</td></tr>
+          <tr><td>2:05PM - GROUP B Match : 2 vs 3</td></tr>
+
         </table>
       </td>
 
@@ -42,7 +72,9 @@
 
       <td> 
         <table>
-          <tr>Marius</tr>
+          <tr>
+            ?
+          </tr>
         </table>
       </td>
     </tr>
